70025b0d96cb58604247260efdbddcfd8f107fce
m_moslehikhou <m_moslehikhou@isc.co.ir>
2024-10-08 15:57:34 +0330

    add page not found

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
 import './NotFound.css'
+import { Link } from '@mui/material'
 
 export default function NotFound() {
-  return <h2>Page Not Found!</h2>
+  return (
+    <div id='notfound'>
+      <div className='notfound'>
+        <h2>404 - Page not found</h2>
+        <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
+        <Link href={'/app/credentials'}>home page</Link>
+      </div>
+    </div>
+  )
 }
