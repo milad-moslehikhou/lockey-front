678474c8a1f596f9c02c93d7fbb61b389ca95e35
m_moslehikhou <m_moslehikhou@isc.co.ir>
2024-10-10 11:09:25 +0330

    fix user edit form autocomplete field

--- src/components/User/UserEditForm.tsx
+++ src/components/User/UserEditForm.tsx
@@ -4,7 +4,7 @@ import { useDispatch } from 'react-redux'
 import { useForm, Controller } from 'react-hook-form'
 import { TextField, FormControlLabel, Switch, FormControl, Box, Button, Avatar } from '@mui/material'
 import FormDialog from '../FormDialog/FormDialog'
-import { useEditUserMutation, useGetGroupsQuery } from '../../features/apiSlice'
+import { useEditUserMutation, useGetGroupsQuery, useGetPermissionsQuery } from '../../features/apiSlice'
 import useSnackbar from '../../hooks/useSnackbar'
 import type { UserType } from '../../types/user'
 import { userActions } from '../../features/userSlice'
@@ -17,16 +17,15 @@ import styled from '@emotion/styled'
 interface UserEditFormPropsType {
   user: UserType
 }
-interface UserEditFromType extends UserType {
-  groupsOption: AutoCompleteFieldOptionsType[]
-}
 
 const UserEditForm = ({ user }: UserEditFormPropsType) => {
   const dispatch = useDispatch()
   const openSnackbar = useSnackbar()
   const loggedInUser = useLoggedInUser()
   const { data: groups, isLoading: groupsIsLoading } = useGetGroupsQuery()
+  const { data: perms, isLoading: permsIsLoading } = useGetPermissionsQuery()
   const [groupOptions, setGroupOptions] = React.useState<AutoCompleteFieldOptionsType[]>([])
+  const [permOptions, setPermOptions] = React.useState<AutoCompleteFieldOptionsType[]>([])
   const [edit, { isLoading: editUserIsLoading }] = useEditUserMutation()
 
   const {
@@ -37,7 +36,7 @@ const UserEditForm = ({ user }: UserEditFormPropsType) => {
     setValue,
     setError,
     formState: { errors },
-  } = useForm<Partial<UserEditFromType>>({
+  } = useForm<Partial<UserType>>({
     defaultValues: {
       ...user,
     },
@@ -45,7 +44,7 @@ const UserEditForm = ({ user }: UserEditFormPropsType) => {
   const handleCloseForm = () => {
     dispatch(userActions.setShowForms({ edit: false }))
   }
-  const handleOnAutoCompleteValueChange = (newValue: AutoCompleteFieldOptionsType[]) => {
+  const handleOnGroupsValueChange = (newValue: AutoCompleteFieldOptionsType[]) => {
     if (groups) {
       const selectOptions = newValue.map(v => v.value)
       const filteredUsers = groups.filter(
@@ -57,12 +56,26 @@ const UserEditForm = ({ user }: UserEditFormPropsType) => {
         })
       )
     }
-    setValue('groupsOption', newValue)
+    setValue('groups', newValue)
+  }
+  const handleOnPermsValueChange = (newValue: AutoCompleteFieldOptionsType[]) => {
+    if (perms) {
+      const selectOptions = newValue.map(v => v.value)
+      const filteredUsers = perms.filter(u => !selectOptions.includes(u.id))
+      setPermOptions(
+        filteredUsers.map(f => {
+          return { label: f.codename, value: f.id }
+        })
+      )
+    }
+    setValue('user_permissions', newValue)
   }
   const handleOnGroupChange = (event: React.SyntheticEvent, newValue: AutoCompleteFieldOptionsType[]) => {
-    handleOnAutoCompleteValueChange(newValue)
+    handleOnGroupsValueChange(newValue)
   }
-
+  const handleOnPermChange = (event: React.SyntheticEvent, newValue: AutoCompleteFieldOptionsType[]) => {
+    handleOnPermsValueChange(newValue)
+  }
   const handleOnAvatarChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target && event.target.files && event.target.files.length > 0) {
       setValue('avatar', event.target.files[0])
@@ -72,7 +85,8 @@ const UserEditForm = ({ user }: UserEditFormPropsType) => {
   const onSubmit = async (data: Partial<UserType>) => {
     data = {
       ...data,
-      groups: groupOptions.map(g => g.value),
+      groups: data.groups && data.groups.map(g => (typeof g === 'number' ? g : g.value)),
+      user_permissions: data.user_permissions && data.user_permissions.map(p => (typeof p === 'number' ? p : p.value)),
     }
 
     try {
@@ -97,20 +111,38 @@ const UserEditForm = ({ user }: UserEditFormPropsType) => {
 
   React.useEffect(() => {
     register('groups')
+    register('user_permissions')
     register('avatar')
   }, [register])
 
   React.useEffect(() => {
-    setGroupOptions(
-      groups
-        ? groups.map(g => {
-            return { label: g.name, value: g.id }
-          })
-        : []
-    )
+    if (!groupsIsLoading && groups) {
+      const groupOptions: AutoCompleteFieldOptionsType[] = []
+      user.groups
+        .map(ug => groups.find(g => g.id === ug))
+        .forEach(fg => {
+          if (fg) groupOptions.push({ label: fg.name, value: fg.id })
+        })
+      handleOnGroupsValueChange(groupOptions)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [groups])
 
+  React.useEffect(() => {
+    if (!permsIsLoading && perms) {
+      const findedPerm: AutoCompleteFieldOptionsType[] = []
+      user.user_permissions
+        .map(up => perms.find(p => p.id === up))
+        .forEach(fp => {
+          if (fp) findedPerm.push({ label: fp.codename, value: fp.id })
+        })
+      handleOnPermsValueChange(findedPerm)
+    } else handleOnPermsValueChange([])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [perms])
+
   const groupsSelectedValue = watch('groups')
+  const permsSelectedValue = watch('user_permissions')
   const avatarValue = watch('avatar', user.avatar) || ''
 
   const VisuallyHiddenInput = styled('input')({
@@ -231,6 +263,19 @@ const UserEditForm = ({ user }: UserEditFormPropsType) => {
         sx={{ mt: 2 }}
       >
         <AutoCompleteField
+          label='Permissions'
+          value={permsSelectedValue}
+          error={'user_permissions' in errors}
+          helperText={errors.user_permissions && (errors.user_permissions.message as string)}
+          options={permOptions}
+          onChange={handleOnPermChange}
+        />
+      </FormControl>
+      <FormControl
+        fullWidth
+        sx={{ mt: 2 }}
+      >
+        <AutoCompleteField
           label='Group'
           value={groupsSelectedValue}
           error={'groups' in errors}
@@ -247,7 +292,7 @@ const UserEditForm = ({ user }: UserEditFormPropsType) => {
       title='Edit User'
       form={form}
       submitLable='Apply'
-      isLoading={editUserIsLoading || groupsIsLoading}
+      isLoading={editUserIsLoading || groupsIsLoading || permsIsLoading}
       handleSubmit={handleSubmit(onSubmit)}
       handleCloseForm={handleCloseForm}
     />
